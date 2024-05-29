3b7b57ddde87165ce51c2631cbc6ef5ecc7eafc4
Nicholas Nguyen <nicholas.nguyen@chargeitspot.com>
2024-05-29 08:22:10 -0400

    adding bikes over time graph

--- saturday_vs_sunday_bikes.py
+++ saturday_vs_sunday_bikes.py
@@ -6,15 +6,16 @@ from matplotlib.ticker import AutoLocator
 def main():
     saturday = 2
     sunday = 3
-    columns = ["AM8", "AM9", "AM10", "AM11",]
     dataframe = pd.read_csv("18_19_spruce_bike_traffic.csv")
     saturday_bikes = dataframe.iloc[saturday]
     sunday_bikes = dataframe.iloc[sunday]
+
+    """
+    columns = ["AM8", "AM9", "AM10", "AM11",]
     saturday_bikes_between_8_and_11 = sum(saturday_bikes[columns])
     sunday_bikes_between_8_and_11 = sum(sunday_bikes[columns])
     days = ["Saturday", "Sunday"]
     day_vs_day_bikes = [saturday_bikes_between_8_and_11, sunday_bikes_between_8_and_11]
-    """
     plt.bar(days, day_vs_day_bikes)
     plt.title("Number of bikes between 18th and 19th and Spruce \n between 8am and 11am on Saturday vs Sunday (2021)")
     plt.savefig("18th_and_19th_spruce_bikes.png")
@@ -33,8 +34,6 @@ def main():
     plt.gca().xaxis.set_major_locator(AutoLocator())
     plt.savefig("bikes_over_time.png")
     plt.show()
-
-
 
 
 if __name__ == '__main__':
